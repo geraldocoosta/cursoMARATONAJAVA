53d438dd16e2ff9b85afa56b0eaaa718b0c912a6
Geraldo Costa <geraldocoosta@gmail.com>
2018-08-15 10:08:08 -0300

    Alteração principal: /Exercicio/src/br/com/ultcode/datetime/test/Teste2.java

--- src/br/com/ultcode/datetime/test/Teste2.java
+++ src/br/com/ultcode/datetime/test/Teste2.java
@@ -1,11 +1,92 @@
 package br.com.ultcode.datetime.test;
 
+import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 public class Teste2 {
 	public static void main(String[] args) {
-		System.out.println("Instant ===================================================");
+		System.out.println("Instant \t===================================================\n");
+
+		// Inicialização
 		Instant t = Instant.now();
-		System.out.println(t);
+		System.out.println(t); // -> instant sempre vai corresponder ao zulu time
+		System.out.println(Instant.EPOCH); // -> data onde começa a contar os segundos do Instant
+		// Iniciando um Instant a partir de Millisegundos
+		System.out.println(Instant.ofEpochMilli(System.currentTimeMillis()));
+		// Parse bem tilindinho
+		System.out.println(Instant.parse("2016-12-16T23:15:30.00Z"));
+		// Criando a partir de segundos com ajustes em nanosegundos
+		// -> a partir de 1970-1-1
+		System.out.println(Instant.ofEpochSecond(99999999900L, 1_000_000_000));
+		System.out.println(Instant.ofEpochSecond(99999999L, -1_000_000_000));
+
+		// Método legais
+		System.out.println(t.getEpochSecond());
+		System.out.println(t.getNano());
+		System.out.println(t.toEpochMilli());
+		// -> temos o plus -> aumentar horas
+		// -> temos o minus -> diminuir horas
+		// -> temos o with -> substituir horas
+		System.out.println("\nDuration \t===================================================\n");
+		// Podemos trabalhar pra pegar intervalos em horas, minutos e segundos -> TEM A VER COM TEMPO- TEEEEEEEEEEEEMPO
+		// HORA - MINUTO - SEGUNDO
+		LocalDateTime dt1 = LocalDateTime.now();
+		LocalDateTime dt2 = LocalDateTime.of(2016, 12, 16, 23, 12, 53);
+
+		LocalTime time1 = LocalTime.now();
+		LocalTime time2 = LocalTime.of(0, 30);
+
+		// O método between recebe -> (Temporal startInclusive, Temporal endExclusive)
+		Duration d1 = Duration.between(dt1, dt2);
+		System.out.println(d1); // -> a formação fica estranha
+		Duration d2 = Duration.between(time1, time2);
+		System.out.println(d2);
+		Duration d3 = Duration.between(Instant.now(), Instant.now().plusSeconds(45600));
+		System.out.println(d3);
+		Duration d4 = Duration.between(dt1, time2.atDate(dt1.toLocalDate()));
+		System.out.println(d4);
+		Duration d5 = Duration.ofMinutes(3);
+		System.out.println(d5);
+		Duration d6 = Duration.of(4, ChronoUnit.MICROS); // não suporta meses, anos, semanas, forever(?), eras, decadas, milenias
+		System.out.println(d6);
+
+		// OBS.: não utilize LocalDate e não misture as classes comparadas, vai rolar uma
+		// exceção bem concentrada
+
+		System.out.println("\nPeriod \t\t===================================================\n");
+		// Nessa aqui trabalhamos com dias, meses, anos
+		// PARAMS DO BETWEEN -> (LocalDate startDateInclusive, LocalDate endDateExclusive)
+		// repare no LOCALDATE
+		// ANO - MES - DIA
+		Period p1 = Period.between(dt1.toLocalDate(), dt2.toLocalDate());
+		System.out.println(p1);
+		Period p2 = Period.ofDays(322);
+		System.out.println(p2);
+		Period p3 = Period.from(p2);
+		System.out.println(p3);
+		Period p4 = Period.of(2015, Month.APRIL.getValue(), 23);
+		System.out.println(p4);
+		// ATENÇÃO ATENÇÃO -> NA CLASSE PERIOD, A DATA É ORGANIZADA EM ANO-MES-DIA, CADA UM NO SEU CAMPO
+		// E O Q Q TEM GERALDÃO? SE VOCÊ PEDIR TU PASSAR UMA DATA EM DIA, AI TU ME PEDE MÊS, DEU BIGODE
+		// EXEMPLO
+		System.out.println(p2.getMonths()); // Passei no ofDays 322 dias, porém aqui retornou 0 meses
+		LocalDateTime now = LocalDateTime.now();
+		// until -> até
+		System.out.println(now.until(now.plusDays(p2.getDays()), ChronoUnit.MONTHS));
+
+		System.out.println("\nChronoUnit \t===================================================\n");
+		// Para esse calculo, me parece q o ChronoUnit é o melhor, pelo menos pra saber sobre só um param
+		LocalDateTime aniversario = LocalDateTime.of(1997, Month.MAY, 23, 6, 30, 12);
+		System.out.println(ChronoUnit.DAYS.between(aniversario, LocalDateTime.now()));
+		System.out.println(ChronoUnit.MONTHS.between(aniversario, LocalDateTime.now()));
+		System.out.println(ChronoUnit.WEEKS.between(aniversario, LocalDateTime.now()));
+		System.out.println(ChronoUnit.YEARS.between(aniversario, LocalDateTime.now()));
+		System.out.println(ChronoUnit.SECONDS.between(aniversario, LocalDateTime.now()));
+
 	}
 }
--- src/br/com/ultcode/threads/teste/MetodosSleepYieldJoin.java
+++ src/br/com/ultcode/threads/teste/MetodosSleepYieldJoin.java
@@ -15,24 +15,24 @@ class ThreadsExemplo implements Runnable {
 	}
 }
 
-// sleep - espera um determindado tempo. não garante que uma thread voltará a executar
+// sleep - espera um determindado tempo. nï¿½o garante que uma thread voltarï¿½ a executar
 // depois que o tempo passar, garante que ela vai retornar ao estado de runnable
 // wait - espera um notify ou notifyall de outro Thread.
-// yield - eventualmente permite outros Threads a executar. Não é garantido, mas uma das
-// funções desse método é dar uma chance de threads com prioridade igual ou superior serem
+// yield - eventualmente permite outros Threads a executar. Nï¿½o ï¿½ garantido, mas uma das
+// funï¿½ï¿½es desse mï¿½todo ï¿½ dar uma chance de threads com prioridade igual ou superior serem
 // executadas, voltando a thread em running para o estado runnable
-// join - método não statico utilizado para avisar uma thread, que outra thread que ela chamou
+// join - mï¿½todo nï¿½o statico utilizado para avisar uma thread, que outra thread que ela chamou
 // deve terminar seu run antes de chamar uma thread diferente, ou antes de completar seu proprio
 // run
-// exemplo -> thread main que manda executar as threads T1 e T2, então temos que falar
-// pra thread main executar t2 só quando t1 acabar, [unico garantido!!!]
-// Não importa a prioridade, o join vai executar e vai ficar suna suna
-// porém, se startar e depois dar o join, provavelmente a jvm vai começar executando na normalidade
-// ai quando der o join ela vai dar a prioridade bolada, porém ainda tem possibilidade de aparecer
+// exemplo -> thread main que manda executar as threads T1 e T2, entï¿½o temos que falar
+// pra thread main executar t2 sï¿½ quando t1 acabar, [unico garantido!!!]
+// Nï¿½o importa a prioridade, o join vai executar e vai ficar suna suna
+// porï¿½m, se startar e depois dar o join, provavelmente a jvm vai comeï¿½ar executando na normalidade
+// ai quando der o join ela vai dar a prioridade bolada, porï¿½m ainda tem possibilidade de aparecer
 // outra thread, fique ligeiro
 
 
-public class MétodosSleepYieldJoin {
+public class MetodosSleepYieldJoin {
 	public static void main(String[] args) throws InterruptedException {
 		Thread t1 = new Thread(new ThreadsExemplo("NORMAL1"), "Thread1");
 		Thread t2 = new Thread(new ThreadsExemplo("NORMAL2"), "Thread2");
@@ -44,7 +44,7 @@ public class MétodosSleepYieldJoin {
 		t2.start();
 		t3.start();
 		t1.join(); // o que estamos falando aqui?
-		// thread que executa a thread t1, marca suave até ela terminar o serviço dela,
+		// thread que executa a thread t1, marca suave atï¿½ ela terminar o serviï¿½o dela,
 		// depois chama os outros otarios
 		
 	}
